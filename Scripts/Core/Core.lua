ebc003737d7094727f164c0de557ea4c70ab19d5
Tsusai <Tsusai@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2007-04-23 01:16:24 +0000

    - Rev 2 - [2007/04/22] Tsusai - Added Core folder with Core.lua and NPC_ID.lua [2007/04/22] Tsusai - Added MapLoaders directory. -

---
+++ Scripts/Core/Core.lua
@@ -0,0 +1,14 @@
+----------------------------------------------------------
+--Core.lua                                              --
+----------------------------------------------------------
+
+-- This file loads the common constants and functions and
+-- is loaded on server startup.  They are passed on to 
+-- descendant lua instances.
+-- 
+
+-- NPC Sprite values
+   require("Scripts/Core/NPC_ID")
+
+-- Job IDs
+-- require("Scripts/Core/JobID")
