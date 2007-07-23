2b2c379eb1838efcd8c77866bca1c7a909d83178
tsusai <5499224+Tsusai@users.noreply.github.com>
2007-07-23 15:25:05 +0000

    - Rev 10 - [2007/07/22] Tsusai - Added a sample script for zeny subtraction and addition (reflects server rev 294) -

---
+++ Scripts/Sample/Zeny.lua
@@ -0,0 +1,12 @@
+npc("new_1-1","Zeny NPC",spr_4_F_JOB_HUNTER,53,115,4,0,0,"new_1_1_Zeny_NPC_53115")
+function new_1_1_Zeny_NPC_53115()
+	dialog "Hello"
+	dialog "I'll attempt to eat 50z"
+	wait()
+	getgold(-50)
+	dialog "Eaten..now to give it back"
+	wait()
+	getgold(50)
+	dialog "Returned, bye"
+	close()
+end
