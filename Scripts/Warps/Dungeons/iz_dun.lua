b8728849198afa3a5ec200e5b3342f871e5ae260
RabidCh <RabidCh@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2007-11-27 05:34:25 +0000

    - Rev 12 - [2007/11/26] RabidCh - Merged izlude_link.sc from Yaros with izlude_town, prontera_fields, iz_dun. [2007/11/26] RabidCh - Fixed previous change dates on the warp scripts from Rev 11. -

--- Scripts/Warps/Dungeons/iz_dun.lua
+++ Scripts/Warps/Dungeons/iz_dun.lua
@@ -12,14 +12,55 @@
 -- |-------------------------------------------------------------|
 -- | Current Version: 1.0      | Episode 10.3 File.              |
 -- |-------------------------------------------------------------|
--- | 11/26/2007 : iz_dun Created. [RabidChocobo]                 |
+-- | 26/11/2007 : iz_dun Created. [RabidCh]                      |
+-- | 26/11/2007 : iz_dun warps added from izlude_link [RabidCh]  |
 -- |-------------------------------------------------------------|
 -- | Credits: Gravity                                            |
 -- |-------------------------------------------------------------|
--- | Notes: Converted to Helios by RabidChocobo with Tsusai's    |
+-- | Notes: Converted to Helios by RabidCh with Tsusai's         |
 -- | Aegis Script Converter                                      |
 -- |                                                             |
 -- \____________________________________________________________/
+
+warp("izlu2dun","Field_Dungeon",108,83,3,2)
+function Field_Dungeon()
+	moveto("iz_dun00",168,168)
+end
+
+warp("iz_dun00","Dungeon_Field",168,173,3,3)
+function Dungeon_Field()
+	moveto("izlu2dun",108,88)
+end
+
+warp("iz_dun00","Iz_Dungeon1_2",39,41,5,2)
+function Iz_Dungeon1_2()
+	moveto("iz_dun01",41,37)
+end
+
+warp("iz_dun01","Iz_Dungeon2_1",41,32,2,2)
+function Iz_Dungeon2_1()
+	moveto("iz_dun00",39,46)
+end
+
+warp("iz_dun00","Iz_Dungeon1_2_",352,342,5,2)
+function Iz_Dungeon1_2_()
+	moveto("iz_dun01",253,252)
+end
+
+warp("iz_dun01","Iz_Dungeon2_1_",253,258,2,2)
+function Iz_Dungeon2_1_()
+	moveto("iz_dun00",352,337)
+end
+
+warp("iz_dun01","Iz_Dungeon2_3",118,170,5,2)
+function Iz_Dungeon2_3()
+	moveto("iz_dun02",236,204)
+end
+
+warp("iz_dun02","Iz_Dungeon3_2",236,198,5,3)
+function Iz_Dungeon3_2()
+	moveto("iz_dun01",118,165)
+end
 
 warp("iz_dun02","Iz_Dungeon3_4",339,331,2,2)
 function Iz_Dungeon3_4()
@@ -31,13 +72,13 @@ function Iz_Dungeon4_3()
 	moveto("iz_dun02",339,328)
 end
 
-warp("iz_dun03","Iz_Dungeon3_4",264,245,1,2)
-function Iz_Dungeon3_4()
+warp("iz_dun03","Iz_Dungeon4_5",264,245,1,2)
+function Iz_Dungeon4_5()
 	moveto("iz_dun04",26,27)
 end
 
-warp("iz_dun04","Iz_Dungeon4_3",26,24,2,2)
-function Iz_Dungeon4_3()
+warp("iz_dun04","Iz_Dungeon5_4",26,24,2,2)
+function Iz_Dungeon5_4()
 	moveto("iz_dun03",261,245)
 end
 
--- Scripts/Warps/Towns/izlude_town.lua
+++ Scripts/Warps/Towns/izlude_town.lua
@@ -12,11 +12,15 @@
 -- |-------------------------------------------------------------|
 -- | Current Version: 1.0      | Episode 10.3 File.              |
 -- |-------------------------------------------------------------|
--- | 11/26/2007 : izlude_town Created. [RabidChocobo]            |
+-- | 28/11/2006 : Added 1st Version. [Muad_Dib]                  |
+-- | 26/11/2007 : izlude_town Created from izlude_link [RabidCh] |
+-- | 26/11/2007 : Put iz_dun in Warps/Dungeons &                 |
+-- | prt_fild08 -> izlude warp in Warps/Fields in                |
+-- | prontera_fields. [RabidCh]                                  |
 -- |-------------------------------------------------------------|
 -- | Credits: Gravity                                            |
 -- |-------------------------------------------------------------|
--- | Notes: Converted to Helios by RabidChocobo with Tsusai's    |
+-- | Notes: Converted to Helios by RabidCh with Tsusai's         |
 -- | Aegis Script Converter                                      |
 -- |                                                             |
 -- \____________________________________________________________/
@@ -26,4 +30,84 @@ function sword_out()
 	checkpoint("izlude_in",68,165)
 	moveto("izlude_in",68,165)
 end
-	
+
+warp("izlude","Iz_Field",30,78,3,3)
+function Iz_Field()
+	moveto("prt_fild08",367,212)
+end
+
+warp("izlude","Kit_Shopi",148,148,2,2)
+function Kit_Shopi()
+	moveto("izlude_in",116,49)
+end
+
+warp("izlude_in","Kit_Shopo",116,46,3,2)
+function Kit_Shopo()
+	moveto("izlude",145,145)
+end
+
+warp("izlude","Armoryi",109,151,3,3)
+function Armoryi()
+	moveto("izlude_in",65,87)
+end
+
+warp("izlude_in","Armoryo",65,84,5,2)
+function Armoryo()
+	moveto("izlude",113,147)
+end
+
+warp("izlude","Swordman_Associationi",52,140,2,2)
+function Swordman_Associationi()
+	moveto("izlude_in",74,161)
+end
+
+warp("izlude_in","Swordman_Associationo",74,158,3,2)
+function Swordman_Associationo()
+	moveto("izlude",52,136)
+end
+
+warp("izlude_in","Swordman_AssociationL__L",43,169,2,3)
+function Swordman_AssociationL__L()
+	moveto("izlude_in",63,169)
+end
+
+warp("izlude_in","Swordman_AssociationR_R_",87,169,2,3)
+function Swordman_AssociationR_R_()
+	moveto("izlude_in",111,169)
+end
+
+warp("izlude_in","Swordman_AssociationR__R",108,169,2,3)
+function Swordman_AssociationR__R()
+	moveto("izlude_in",84,169)
+end
+
+warp("izlude","Swordman_Associationi",216,129,3,2)
+function Swordman_Associationi()
+	moveto("izlude_in",151,127)
+end
+
+warp("izlude_in","Swordman_Associationo",148,127,2,5)
+function Swordman_Associationo()
+	moveto("izlude",212,129)
+end
+
+warp("izlude_in","Swordman_AssociationL_L_",172,139,3,2)
+function Swordman_AssociationL_L_()
+	moveto("izlude_in",172,161)
+end
+
+warp("izlude_in","Swordman_AssociationL__L",172,158,3,2)
+function Swordman_AssociationL__L()
+	moveto("izlude_in",172,136)
+end
+
+warp("izlude_in","Swordman_AssociationR_R_",172,116,3,2)
+function Swordman_AssociationR_R_()
+	moveto("izlude_in",172,94)
+end
+
+warp("izlude_in","Swordman_AssociationR__R",171,97,3,2)
+function Swordman_AssociationR__R()
+	moveto("izlude_in",172,119)
+end
+
