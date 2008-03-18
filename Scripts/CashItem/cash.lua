4ca66e130625bb15686611c498c69c0ff025be94
Spre <Spre@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2008-03-18 07:39:47 +0000

    -Rev 49- [2008/3/18] Spre - Added CashItem scripts from Yaros 11.1? Note Helios doesn't have their functions yet. -

---
+++ Scripts/CashItem/cash.lua
@@ -0,0 +1,46 @@
+-- _________________________________________________ --
+--/                                                 \--
+--|     _    _          _   _                       |--
+--|    | |  | |        | | (_)                      |--
+--|    | |__| |   ___  | |  _    ___    ___         |--
+--|    |  __  |  / _ \ | | | |  / _ \  / __|        |--
+--|    | |  | | |  __/ | | | | | (_) | \__ \        |--
+--|    |_|  |_|  \___| |_| |_|  \___/  |___/        |--
+--|                                                 |--
+--|-------------------------------------------------|--
+--| 2008/03/18                                      |--
+--|   -First Version for Helios  [Spre]             |--
+--|-------------------------------------------------|--
+--| Credits: Gravity                                |--
+--|          Yaros                                  |--
+--|-------------------------------------------------|--
+--| Notes:                                          |--
+--|        No Function In helios yet.               |--
+--|                                                 |--
+--\_________________________________________________/--
+
+
+CashTrader("prontera","iMall Owner","1_M_PAY_ELDER",151,133,4,1,1)
+OnInit:()
+	CashShop "FIRST"
+end
+
+CashTrader("payon","iMall Owner","1_M_PAY_ELDER",200,125,4,1,1)
+OnInit:()
+	CashShop "SECOND"
+end
+
+CashTrader("morocc","iMall Owner","1_M_PAY_ELDER",169,73,4,1,1)
+OnInit:()
+	CashShop "THIRD"
+end
+
+CashTrader("alberta","iMall Owner","1_M_PAY_ELDER",46,37,4,1,1)
+OnInit:()
+	CashShop "FOURTH"
+end
+
+CashTrader("aldebaran","iMall Owner","1_M_PAY_ELDER",116,106,4,1,1)
+OnInit:()
+	CashShop "FIFTH"
+end
