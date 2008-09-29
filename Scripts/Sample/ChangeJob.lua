535e4ddfaa626a5c2329bb41ddd1e23a0ab4eb18
Spre <Spre@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2008-09-29 02:52:08 +0000

    -Rev 66- [2008/09/28] Spre -  LOL rax... Fixed JobChange sample it AND menu/vars both load and are in different areas!. -

--- Scripts/Sample/ChangeJob.lua
+++ Scripts/Sample/ChangeJob.lua
@@ -1,4 +1,4 @@
-npc("new_1-1","Menu and Var",SPRITE_4_F_JOB_KNIGHT,54,120,4,0,0,"new_1_1_ChangeJob_54115")
+npc("new_1-1","ChangeJob",SPRITE_4_F_JOB_KNIGHT,51,120,4,0,0,"new_1_1_ChangeJob_54115")
 function new_1_1_ChangeJob_54115()
 	dialog "Hello"
 	dialog "You are now a swordman!"
