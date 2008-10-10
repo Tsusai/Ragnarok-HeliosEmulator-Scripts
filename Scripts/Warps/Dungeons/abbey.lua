72bfe8676dd01a07c269d859482f529f11032fa2
RabidCh <RabidCh@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2008-10-10 22:24:41 +0000

    -Rev 68- [2008/10/10] RabidCh - Added Nameless Island and Abbey Dungeon warps.  Thanks to LimitLine and eA. -

---
+++ Scripts/Warps/Dungeons/abbey.lua
@@ -0,0 +1,55 @@
+-- _________________________________________________ --
+--/                                                 \--
+--|    _    _          _   _                        |--
+--|   | |  | |        | | (_)                       |--
+--|   | |__| |   ___  | |  _    ___    ___          |--
+--|   |  __  |  / _ \ | | | |  / _ \  / __|         |--
+--|   | |  | | |  __/ | | | | | (_) | \__ \         |--
+--|   |_|  |_|  \___| |_| |_|  \___/  |___/         |--
+--|                                                 |--
+--|-------------------------------------------------|--
+--| 10/10/2007                                      |--
+--|   - Abbey Dungeon warps created. [RabidCh]      |--
+--|-------------------------------------------------|--
+--| Credits: LimitLine                              |--
+--|          eA?                                    |--
+--|-------------------------------------------------|--
+--| Notes:                                          |--
+--|                                                 |--
+--|                                                 |--
+--\_________________________________________________/--
+
+warp("abbey01","abbeywarp001_519",51,9,2,3)
+function abbeywarp001()
+	moveto("nameless_n",160,184)
+end
+
+warp("abbey01","abbeywarp002_87122",87,122,2,3)
+function abbeywarp002()
+	moveto("abbey01",13,201)
+end
+
+warp("abbey01","abbeywarp003_13204",13,204,2,3)
+function abbeywarp003()
+	moveto("abbey01",88,118)
+end
+
+warp("abbey01","abbeywarp004_321102",321,102,2,3)
+function abbeywarp004()
+	moveto("abbey02",149,11)
+end
+
+warp("abbey02","abbeywarp005_1498",149,8,2,3)
+function abbeywarp005()
+	moveto("abbey01",321,99)
+end
+
+warp("abbey02","abbeywarp006_149290",149,290,2,3)
+function abbeywarp006()
+	moveto("abbey03",119,8)
+end
+
+warp("abbey03","abbeywarp007_1195",119,5,2,3)
+function abbeywarp007()
+	moveto("abbey02",149,287)
+end
