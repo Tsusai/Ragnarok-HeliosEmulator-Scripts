87f63b73bb8279fdda0ac17ab2184d1faec464c4
RabidCh <RabidCh@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2008-10-10 22:24:41 +0000

    -Rev 68- [2008/10/10] RabidCh - Added Nameless Island and Abbey Dungeon warps.  Thanks to LimitLine and eA. -

---
+++ Scripts/Warps/Dungeons/nameless_i.lua
@@ -0,0 +1,55 @@
+-- _________________________________________________ --
+--/                                                 \--
+--|    _    _          _   _                        |--
+--|   | |  | |        | | (_)                       |--
+--|   | |__| |   ___  | |  _    ___    ___          |--
+--|   |  __  |  / _ \ | | | |  / _ \  / __|         |--
+--|   | |  | | |  __/ | | | | | (_) | \__ \         |--
+--|   |_|  |_|  \___| |_| |_|  \___/  |___/         |--
+--|                                                 |--
+--|-------------------------------------------------|--
+--| 10/10/2007                                      |--
+--|   - Nameless Island warps created. [RabidCh]    |--
+--|-------------------------------------------------|--
+--| Credits: LimitLine                              |--
+--|          eA?                                    |--
+--|-------------------------------------------------|--
+--| Notes:                                          |--
+--|                                                 |--
+--|                                                 |--
+--\_________________________________________________/--
+
+warp("nameless_n","namelesswarp001_157184",157,184,2,3)
+function namelesswarp001()
+	moveto("abbey01",51,12)
+end
+
+warp("nameless_i","namelesswarp002_252130",252,130,2,3)
+function namelesswarp002()
+	moveto("nameless_in",26,128)
+end
+
+warp("nameless_in","namelesswarp003_23128",23,128,2,3)
+function namelesswarp003()
+	moveto("nameless_i",249,130)
+end
+
+warp("nameless_i","namelesswarp004_79256",79,256,2,3)
+function namelesswarp004()
+	moveto("nameless_i",12,172)
+end
+
+warp("nameless_in","namelesswarp005_12169",12,169,2,3)
+function namelesswarp005()
+	moveto("nameless_i",82,256)
+end
+
+warp("nameless_i","namelesswarp006_7894",78,94,2,3)
+function namelesswarp006()
+	moveto("nameless_in",96,180)
+end
+
+warp("nameless_in","namelesswarp007_96177",96,177,2,3)
+function namelesswarp007()
+	moveto("nameless_i",81,96)
+end
