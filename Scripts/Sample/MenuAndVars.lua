2264095ad75ce24d537bc3c05d8ed828b51c3d98
OneRaX <OneRaX@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2008-10-21 19:04:45 +0000

    -Rev 75- [2008/10/21] RaX - Modified menu_and_var script under sample to demonstrate typed variables. -

--- Scripts/Sample/MenuAndVars.lua
+++ Scripts/Sample/MenuAndVars.lua
@@ -1,9 +1,17 @@
-npc("new_1-1","Menu and Var",SPRITE_4_F_JOB_HUNTER,54,115,4,0,0,"new_1_1_Menu_and_Var_54115")
+npc("new_1-1","Menu and Var",SPRITE_4_F_JOB_HUNTER,52,115,4,0,0,"new_1_1_Menu_and_Var_54115")
 function new_1_1_Menu_and_Var_54115()
 	dialog "Hello"
 	local temp = getvar("temp")
 	dialog(temp)
-	local mresult = menu(1,2,3,4)
-	setvar("temp",mresult)
+	local mresult = menu("I have a hat",0,"false",4)
+	if mresult == 1 then
+		setvar("temp","I have a hat")
+	elseif mresult == 2 then
+		setvar("temp",0)
+	elseif mresult == 3 then
+		setvar("temp","false")
+	elseif mresult == 4 then
+		setvar("temp",4)
+	end
 	close()
 end
