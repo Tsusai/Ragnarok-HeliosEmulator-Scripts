677a5404b835fad933f76c5ced4bffdee008968e
Spre <Spre@07ef0d02-422e-0410-bfe7-cfaadf2d1ce7>
2010-01-21 02:27:18 +0000

    - Rev 82 - [2010/01/20] Spre - Another Update to The trans Quest. Added the variables. Metheus_Sylphe Should load and function. -

--- Scripts/Job/valkyrie.lua
+++ Scripts/Job/valkyrie.lua
@@ -20,44 +20,52 @@
 --\_________________________________________________/--
 npc("yuno","Metheus Sylphe",SPRITE_2_M_SAGE_B,88,164,5,0,0,"Metheus_Sylphe")
 function Metheus_Sylphe()
-	--Job and level Variable here
-		local temp = getvar("valkyrie_q", 1) --Not sure if this is right...
-			dialog "Metheus Sylphe"
-			dialog "Welcome to the Library of the Schweicherbil Magic Academy."
-			dialog "I assume you have come here"
-			dialog "to read the 'Book of Ymir."
-			wait()
-			dialog "Metheus Sylphe"
-			dialog "Unfortunately, the original copy of the book has been damaged over time. We currently only allow the public to view a copy of the book."
-			wait()
-			dialog "Metheus Sylphe"
-			dialog "Also, in order to preserve the original 'Book of Ymir,' we have decided to accept donations from people who wish to read the copy we have provided."
-			wait()
-			dialog "Metheus Sylphe"
-			dialog "The suggested"
-			dialog "donation amount is"
-			dialog "1,285,000 zeny."
-			wait()
-			local mresult = menu("Donate",0,"Cancel",1)
-			if mresult == 1 then
-				--Gold Check here?
-					getgold(-1285000)
-					setvar("valkyrie_q",1)
-					dialog "[Metheus Sylphe]"
-					dialog "Thank you, your donation will be used for a good cause. You May"
-					dialog "now go in and read the book."
-				--Still needs goldcheck Code with
-				--The decline message when gold isn't enough
-			elseif
+	if(getvar(JOBLEVEL) > 49) then
+		if(getvar(CLEVEL) > 98)) then
+			if(getvar("valkyrie_q", 0) then --Not sure if this is right...
 				dialog "Metheus Sylphe"
-				dialog "Take your time, and"
-				dialog "enjoy your travels."
-				end
-			close()
+				dialog "Welcome to the Library of the Schweicherbil Magic Academy."
+				dialog "I assume you have come here"
+				dialog "to read the 'Book of Ymir."
+				wait()
+				dialog "Metheus Sylphe"
+				dialog "Unfortunately, the original copy of the book has been damaged over time. We currently only allow the public to view a copy of the book."
+				wait()
+				dialog "Metheus Sylphe"
+				dialog "Also, in order to preserve the original 'Book of Ymir,' we have decided to accept donations from people who wish to read the copy we have provided."
+				wait()
+				dialog "Metheus Sylphe"
+				dialog "The suggested"
+				dialog "donation amount is"
+				dialog "1,285,000 zeny."
+				wait()
+				local mresult = menu("Donate",0,"Cancel",1)
+				if(mresult == 1) then
+					if(getvar(MONEY) >= 1285000)
+						getgold(-1285000)
+						setvar("valkyrie_q",1)
+						dialog "[Metheus Sylphe]"
+						dialog "Thank you, your donation will be used for a good cause. You May"
+						dialog "now go in and read the book."
+					else
+						dialog "Metheus Sylphe"
+						dialog "Unfortunately, you don't seem to possess enough zeny at the moment. Please check your funds and come back again."
+						close()
+					end
+				elseif
+					dialog "Metheus Sylphe"
+					dialog "Take your time, and"
+					dialog "enjoy your travels."
+					end
+				close()
+			else
+				dialog "Metheus Sylphe"
+				dialog "Once again, thank you for you generous donation."
+				dialog "Feel free to read a carbon copy of the 'Book of Ymir' at your leisure."
+				close()
 		else
-			dialog "[Metheus Sylphe]"
-			dialog "Once again, thank you for you generous donation."
-			dialog "Feel free to read a carbon copy of the 'Book of Ymir' at your leisure."
+			dialog "Metheus Sylphe"
+			dialog "You must be atleast level 99."
 			close()
 	else
 		dialog "Metheus Sylphe"
